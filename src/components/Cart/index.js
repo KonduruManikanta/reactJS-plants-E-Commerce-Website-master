fc90b8d713d0bd167d78b5e07070d117e6c19a28
ManikantaKonduru <manikantakonduru239@gmail.com>
2024-10-20 21:08:29 +0530

    Add initial project files

---
+++ src/components/Cart/index.js
@@ -0,0 +1,9 @@
+import './index.css'
+
+const Cart = () => (
+  <div>
+    CArt
+  </div>
+)
+
+export default Cart
---
+++ src/components/Pagination/index.js
@@ -0,0 +1,17 @@
+import Header from '../Header'
+import ProductCard from '../ProductCard'
+import Footer from '../Footer'
+import './index.css'
+
+const Pagination = () => {
+
+  return (
+    <div className='pagination-card'>
+      <Header />
+      <ProductCard />
+      <Footer />
+    </div>
+  )
+}
+
+export default Pagination
